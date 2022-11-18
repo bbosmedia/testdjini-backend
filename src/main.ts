d63618676187a048bb297d7deaab228b453d8ea5
bbosmedia <bbosmedia@users.noreply.github.com>
2022-11-19 01:09:40 +0500

    Port Changed

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,6 @@ import { AppModule } from './app.module';
 async function serverStart() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  await app.listen(4444);
+  await app.listen(3000);
 }
 serverStart();
